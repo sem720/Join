371cb94e1029ff5958ea55c8e6292e9e03fe2bf7
Ballast <birgit.leitner@yahoo.de>
2025-02-06 20:55:41 +0100

    Button-Logik uvm
    Button-Logik implementiert; Styles angepasst;

---
+++ addTask/addTask.js
@@ -0,0 +1,56 @@
+let activeButton = null; 
+
+function toggleButtons(clickedButton) {
+  if (activeButton) {
+       deactivateButton(activeButton);   
+  }
+
+  if (activeButton === clickedButton) {
+      activeButton = null;
+      return;
+  }
+  
+  activateButton(clickedButton);
+  activeButton = clickedButton;
+}
+
+function activateButton(button) {
+  button.classList.add('active');
+  changeButtonStyle(button, 'add');
+  changeImageStyle(button, 'add');
+}
+
+function deactivateButton(button) {
+  button.classList.remove('active');
+  changeButtonStyle(button, 'remove');
+  changeImageStyle(button, 'remove');
+}
+
+
+function changeButtonStyle(button, action) {
+  const colorMap = {
+      'urgent': '#FF3D00',
+      'medium': '#FFA800',
+      'low': '#7AE229'
+  };
+
+  const color = colorMap[button.id] || '';
+  
+  if (action === 'add') {
+      button.style.backgroundColor = color;
+      button.style.color = 'white';
+  } else if (action === 'remove') {
+      button.style.backgroundColor = '';
+      button.style.color = '';
+  }
+}
+
+function changeImageStyle(button, action) {
+  const img = button.querySelector('img');
+  
+  if (action === 'add') {
+      img.style.filter = 'brightness(0) invert(1)'; 
+  } else if (action === 'remove') {
+      img.style.filter = ''; 
+  }
+}
