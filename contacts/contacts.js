9d1f2658ab91d66f4ed0eac01bd26c35ad810fc8
mohd-3384 <mohd3384@hotmail.com>
2025-02-13 20:43:29 +0100

    contacts design updated 13.02

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -1,31 +1,30 @@
-const BASE_URL = 'https://join-c8725-default-rtdb.europe-west1.firebasedatabase.app/contacts/';
-const myArr = []
-async function fetchContacts(url) {
-    try {
-        const response = await fetch(url + '.json');
-        const contacts = await response.json();
-        const contactListElement = document.getElementById('contactList');
-        console.log(contacts);
-        myArr.push(contacts)
+// const BASE_URL = 'https://join-c8725-default-rtdb.europe-west1.firebasedatabase.app/contacts/';
+// const myArr = []
+// async function fetchContacts(url) {
+//     try {
+//         const response = await fetch(url + '.json');
+//         const contacts = await response.json();
+//         const contactListElement = document.getElementById('contactList');
+//         console.log(contacts);
+//         myArr.push(contacts)
 
-        myArr.forEach((contact) => {
-            console.log(contact.contact1.email);
-        });
-    } catch (error) {
-        console.error('Fehler beim Fetchen der Kontakte:', error);
-    }
-}
-fetchContacts(BASE_URL)
+//         myArr.forEach((contact) => {
+//             console.log(contact.contact1.email);
+//         });
+//     } catch (error) {
+//         console.error('Fehler beim Fetchen der Kontakte:', error);
+//     }
+// }
+// fetchContacts(BASE_URL)
 
 
 /**
  * Open the new contact overlay.
  */
 function openAddContact() {
-    document.getElementById(`addContactOverlay`).classList.add("overlay");
-    // document.body.classList.add("overlay-overflow-hidden");
-    addContactOverlay.innerHTML += openAddContactTemp()
-    const addContact = document.getElementById("addContact");
+    const addContactOverlay = document.getElementById(`addContactOverlay`);
+    addContactOverlay.classList.add("overlay", "animateRightToLeft");
+    addContactOverlay.innerHTML += openAddContactTemp();
     addContact.addEventListener('click', (e) => {
         e.stopPropagation()
     })
@@ -35,7 +34,9 @@ function openAddContact() {
  * Close the new contact overlay.
  */
 function closeAddContact() {
-    document.getElementById(`addContactOverlay`).classList.remove("overlay");
-    // document.body.classList.remove("overlay-overflow-hidden");
+    const addContactOverlay = document.getElementById(`addContactOverlay`);
+    addContactOverlay.classList.remove("overlay", "animateRightToLeft");
     addContactOverlay.innerHTML = "";
 }
+
+
--- contacts/contactsTemp.js
+++ contacts/contactsTemp.js
@@ -4,36 +4,43 @@ function openAddContactTemp() {
                 <div class="addContact-titles">
                     <img src="../assets/imgs/logo-white-blue.png" />
                     <h1>Add Contact</h1>
-                    <p>Tasks are better with a team</p>
+                    <p>Tasks are better with a team!</p>
                     <div class="blue-line"></div>
                 </div>
+
                 <div class="addContact-container">
                     <div class="close" onclick="closeAddContact()">x</div>
-                    <div class="addContact-content">
-                        <div>
-                            <img class="contact-image" src="../assets/imgs/add-contact.png" />
-                        </div>
-                        <form class="contact-form" onsubmit="">
-                            <div class="inputs-container">
-                                <input required id="newContactName" class="input-standard addContact-input contact-name"
+                    <div>
+                        <img class="contact-image" src="../assets/imgs/add-contact.png" />
+                    </div>
+                    <form class="addContact-content" onsubmit="">
+                        <div class="inputs-container">
+                            <div class="addContact-input">
+                                <input required id="newContactName" class="input-standard contact-name"
                                     placeholder="Name" type="text" />
-                                <input required id="newContactEmail"
-                                    class="input-standard addContact-input contact-email" placeholder="Email"
-                                    type="email" />
-                                <input required id="newContactPhone"
-                                    class="input-standard addContact-input contact-phone" placeholder="Phone"
-                                    type="number" />
+                                <img src="../assets/imgs/input-name.png" alt="">
                             </div>
-                    </div>
-                    <div class="buttons-addContact">
-                        <button type="button" onclick="closeAddContact()" class="btn-bright">
-                            Cancel &nbsp; x
-                        </button>
-                        <button type="submit" class="btn-dark">
-                            Create Contact &nbsp;
-                            <img src="../assets/imgs/check-white.png" />
-                        </button>
-                    </div>
+                            <div class="addContact-input">
+                                <input required id="newContactEmail" class="input-standard contact-email"
+                                    placeholder="Email" type="email" />
+                                <img src="../assets/imgs/input-email.png" alt="">
+                            </div>
+                            <div class="addContact-input">
+                                <input required id="newContactPhone" class="input-standard contact-phone"
+                                    placeholder="Phone" type="tel" />
+                                <img src="../assets/imgs/Input_Phone.png" alt="">
+                            </div>
+                        </div>
+                        
+                        <div class="buttons-addContact">
+                            <button type="button" onclick="closeAddContact()" class="btn-bright">
+                                Cancel &nbsp; x
+                            </button>
+                            <button type="submit" class="btn-dark">
+                                Create Contact &nbsp;
+                                <img src="../assets/imgs/check-white.png" />
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
