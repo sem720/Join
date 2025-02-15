5a6a620772a30a9f67e52ed5338af5be4565766b
mohd-3384 <mohd3384@hotmail.com>
2025-02-15 18:37:27 +0100

    first commit for  privacy policy

---
+++ privacyPolicy/privacy.js
@@ -0,0 +1,12 @@
+/**
+ * Navigates the browser back to the previous page.
+ *
+ * This function uses the `window.history.back()` method to navigate the browser back to the previous page.
+ * If the previous page is not available in the browser's history, the function will not throw an error.
+ *
+ * @function toggleBack
+ * @global
+ */
+function toggleBack() {
+    window.history.back();
+}
