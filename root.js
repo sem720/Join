0e3527c15565c36796dd51632b56c6fbe6bbdf75
sem720 <sem.schwab@gmx.de>
2025-02-26 12:09:41 +0100

    button disabled logic

--- root.js
+++ root.js
@@ -1,1 +1,29 @@
+function executeWithLoadingState(actionFunction) {
+  const buttons = document.querySelectorAll("button");
 
+  buttons.forEach((button) => (button.disabled = true));
+
+  const result = actionFunction();
+
+  if (result instanceof Promise) {
+    result.finally(() => {
+      buttons.forEach((button) => (button.disabled = false));
+    });
+  } else {
+    buttons.forEach((button) => (button.disabled = false));
+  }
+}
+
+function attachLoadingStateToButtons() {
+  document.querySelectorAll("button").forEach((button) => {
+    const originalOnClick = button.onclick;
+    button.onclick = function (event) {
+      if (originalOnClick) {
+        event.preventDefault();
+        executeWithLoadingState(() => originalOnClick.call(button, event));
+      }
+    };
+  });
+}
+
+window.addEventListener("DOMContentLoaded", attachLoadingStateToButtons);
