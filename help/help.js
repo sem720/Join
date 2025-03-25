bee6d54d665fc96c73befde99cbb754d70da5acd
sem720 <sem.schwab@gmx.de>
2025-03-25 22:19:53 +0100

    sidebar bug fix

---
+++ help/help.js
@@ -0,0 +1,15 @@
+function checkNavigationVisibility() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const navContainer = document.getElementById("div-navigation");
+  const navConOut = document.getElementById("div-nav-out");
+
+  if (!user) {
+    navContainer.style.display = "none";
+    navConOut.style.display = "block";
+  } else {
+    navContainer.style.display = "block";
+    navConOut.style.display = "none";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", checkNavigationVisibility);
